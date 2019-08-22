cb75fbd9565fa8fb3d0cc60739fb4524bb2ae65f
sergiodiazl <sergiodiazleguizamon@gmail.com>
2019-08-22 12:45:16 -0300

    authentication for admin features

--- src/App.js
+++ src/App.js
@@ -8,18 +8,22 @@ import {
   Redirect
 } from 'react-router-dom';
 import { ShopConnector } from './shop/ShopConnector';
-import { Admin } from "./admin/Admin";
+import { Admin } from './admin/Admin';
+import { AuthProviderImpl } from './auth/AuthProviderImpl';
+
 export default class App extends Component {
   render() {
     return (
       <Provider store={SportsStoreDataStore}>
-        <Router>
-          <Switch>
-            <Route path="/shop" component={ShopConnector} />
-            <Route path="/admin" component={ Admin } />                    
-            <Redirect to="/shop" />
-          </Switch>
-        </Router>
+        <AuthProviderImpl>
+          <Router>
+            <Switch>
+              <Route path="/shop" component={ShopConnector} />
+              <Route path="/admin" component={Admin} />
+              <Redirect to="/shop" />
+            </Switch>
+          </Router>
+        </AuthProviderImpl>
       </Provider>
     );
   }
